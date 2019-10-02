7f40c7f76e1612e4c2d7d88bf91c4acfb5f81d0a
Evan Otero <Evan.otero19@gmail.com>
2019-10-02 17:21:59 +0000

    integrated basic page with errors

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,13 +5,48 @@ export class Navbar extends React.Component {
 	render() {
 		return (
 			<nav className="navbar navbar-light bg-light mb-3">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+				<div className="header container-fluid">
+					<div className="headertop row">
+						<div className="socialmedialinks col-md-3">
+							<ul className="smlinkimgs" style={{ display: "flex" }}>
+								<li className="smimages">
+									<img src="https://image.flaticon.com/icons/svg/1051/1051258.svg" />
+								</li>
+								<li className="smimages">
+									<img src="https://image.flaticon.com/icons/svg/1051/1051243.svg" />
+								</li>
+								<li className="smimages">
+									<img src="https://image.flaticon.com/icons/svg/1051/1051280.svg" />
+								</li>
+								<li className="smimages">
+									<img src="https://image.flaticon.com/icons/svg/1051/1051262.svg" />
+								</li>
+								<li className="smimages">
+									<img src="https://image.flaticon.com/icons/svg/1051/1051248.svg" />
+								</li>
+							</ul>
+						</div>
+						<div className="swlogo col-md-6">
+							<img id="logo" src="http://pngimg.com/uploads/star_wars_logo/star_wars_logo_PNG36.png" />
+						</div>
+						<div className="utility col-md-3">
+							<div className="searchbar" />
+							<input />
+							<div className="login">Log In // Sign Up </div>
+						</div>
+					</div>
+					<div>
+						<div className="navbar">
+							<div className="navbaritems">
+								<ul style={{ display: "flex" }}>
+									<li className="navbarli">News + Blog</li>;<li className="navbarli">Video</li>;
+									<li className="navbarli">Films</li>;<li className="navbarli">Series</li>;
+									<li className="navbarli">Interactive</li>;<li className="navbarli">Community</li>;
+									<li className="navbarli">Databank</li>;
+								</ul>
+							</div>
+						</div>
+					</div>
 				</div>
 			</nav>
 		);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,10 +2,8 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-import { Socialmedia } from "./component/Socialmedia.js";
-import { Navbaritems } from "./component/Navbaritems.js";
-import { InfoNav } from "./component/InfoNav.js";
-import { Card } from "./component/Card.js";
+import { InfoNav } from "./js/component/InfoNav.js";
+import { Card } from "./src/js/component/Card.js";
 
 export class Home extends React.Component {
 	constructor() {
@@ -46,47 +44,6 @@ export class Home extends React.Component {
 	render() {
 		return (
 			<div>
-				<div className="header container-fluid">
-					<div className="headertop row">
-						<div className="socialmedialinks col-md-3">
-							<ul
-								className="smlinkimgs"
-								style={{ display: "flex" }}>
-								<Socialmedia alpha="https://image.flaticon.com/icons/svg/1051/1051258.svg" />
-								<Socialmedia alpha="https://image.flaticon.com/icons/svg/1051/1051243.svg" />
-								<Socialmedia alpha="https://image.flaticon.com/icons/svg/1051/1051280.svg" />
-								<Socialmedia alpha="https://image.flaticon.com/icons/svg/1051/1051262.svg" />
-								<Socialmedia alpha="https://image.flaticon.com/icons/svg/1051/1051248.svg" />
-							</ul>
-						</div>
-						<div className="swlogo col-md-6">
-							<img
-								id="logo"
-								src="http://pngimg.com/uploads/star_wars_logo/star_wars_logo_PNG36.png"
-							/>
-						</div>
-						<div className="utility col-md-3">
-							<div className="searchbar" />
-							<input />
-							<div className="login">Log In // Sign Up </div>
-						</div>
-					</div>
-					<div>
-						<div className="navbar">
-							<div className="navbaritems">
-								<ul style={{ display: "flex" }}>
-									<Navbaritems bravo="News + Blog" />
-									<Navbaritems bravo="Video" />
-									<Navbaritems bravo="Films" />
-									<Navbaritems bravo="Series" />
-									<Navbaritems bravo="Interactive" />
-									<Navbaritems bravo="Community" />
-									<Navbaritems bravo="Databank" />
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
 				<div className="content container">
 					<div className="maincontent">
 						<div className="browsedatabank">Browse Databank //</div>
@@ -103,13 +60,8 @@ export class Home extends React.Component {
 									{this.state.delta.map((foxtrot, golf) => {
 										return (
 											<Card
-												echo={
-													this.state.delta[golf].name
-												}
-												mike={
-													this.state.delta[golf]
-														.height
-												}
+												echo={this.state.delta[golf].name}
+												mike={this.state.delta[golf].height}
 												november="Name:"
 												oscar="Height:"
 												papa="cm"
@@ -122,13 +74,8 @@ export class Home extends React.Component {
 									{this.state.delta.map((kilo, lima) => {
 										return (
 											<Card
-												echo={
-													this.state.juliet[lima].name
-												}
-												mike={
-													this.state.juliet[lima]
-														.population
-												}
+												echo={this.state.juliet[lima].name}
+												mike={this.state.juliet[lima].population}
 												november="Name:"
 												oscar="Population:"
 												key={lima}
